791f3b0ab6270712bdf7d7f2037a410994568d25
weiqiang <409655689@qq.com>
2021-08-08 02:50:00 +0800

    visitor pattern

---
+++ DesignPattern/src/main/java/visitor/Fail.java
@@ -0,0 +1,13 @@
+package visitor;
+
+public class Fail extends Action{
+    @Override
+    public void getManResult(Man man) {
+        System.out.println("man fail");
+    }
+
+    @Override
+    public void getWomanResult(Woman woman) {
+        System.out.println("woman fail");
+    }
+}
