467874fc00cada44e066a449333f7a372be2eba2
weiqiang <409655689@qq.com>
2021-08-09 17:01:39 +0800

    get prime

---
+++ Algorithm/src/test/PrimeTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.Test;
+import prime.Prime;
+
+public class PrimeTest {
+
+    @Test
+    public void testPrimeBasic() {
+        Prime prime = new Prime();
+        final boolean isPrime = prime.isPrimeBasic(151);
+        System.out.println(isPrime);
+    }
+
+    @Test
+    public void testPrimeSquareRoot() {
+        final Prime prime = new Prime();
+        final boolean isPrime = prime.isPrimeSquareRoot(152);
+        System.out.println(isPrime);
+    }
+}
