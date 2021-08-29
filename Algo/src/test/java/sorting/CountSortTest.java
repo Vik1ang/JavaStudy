eacf654c2221d379b64a0146d06f3501cfa4b502
Vik1ang <409655689@qq.com>
2021-08-29 13:17:57 +0800

    count sort

---
+++ Algo/src/test/java/sorting/CountSortTest.java
@@ -0,0 +1,33 @@
+package sorting;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sorting.countSort.CountSort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class CountSortTest {
+    private int[] arr;
+
+    @BeforeEach
+    public void initArray() {
+        Random random = new Random();
+        arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(1000);
+        }
+        System.out.println("Before: " + Arrays.toString(arr));
+    }
+
+    @AfterEach
+    public void printArray() {
+        System.out.println("After: " + Arrays.toString(arr));
+    }
+
+    @Test
+    public void testCountSort() {
+        new CountSort().countSort(arr, 1000);
+    }
+}
