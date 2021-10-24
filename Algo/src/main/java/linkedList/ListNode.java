d0d5f3bc0b979c05cb3fe87381aa8719ddcf7c89
Vik1ang <vik1ang@outlook.com>
2021-10-24 18:19:52 +0800

    let field can be accessible

--- Algo/src/main/java/linkedList/ListNode.java
+++ Algo/src/main/java/linkedList/ListNode.java
@@ -6,8 +6,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     public ListNode(int val) {
         this.val = val;
