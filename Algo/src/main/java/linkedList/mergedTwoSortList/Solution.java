1b8cb2615479dcd06bd47beed486387255c2f136
Vik1ang <vik1ang@outlook.com>
2021-10-24 18:20:08 +0800

    merge two sort linked list

---
+++ Algo/src/main/java/linkedList/mergedTwoSortList/Solution.java
@@ -0,0 +1,11 @@
+package linkedList.mergedTwoSortList;
+
+// https://leetcode-cn.com/problems/merge-two-sorted-lists/
+
+import linkedList.ListNode;
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
