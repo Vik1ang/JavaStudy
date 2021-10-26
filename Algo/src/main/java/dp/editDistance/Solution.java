0c2162e55dc75626f938d8b4239255ccf8a9960d
Vik1ang <vik1ang@outlook.com>
2021-10-26 11:57:40 +0800

    edit distance

---
+++ Algo/src/main/java/dp/editDistance/Solution.java
@@ -0,0 +1,9 @@
+package dp.editDistance;
+
+// https://leetcode-cn.com/problems/edit-distance/
+
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        return -1;
+    }
+}
---
+++ Algo/src/main/java/dp/editDistance/Solution1.java
@@ -0,0 +1,35 @@
+package dp.editDistance;
+
+// https://leetcode-cn.com/problems/edit-distance/
+
+public class Solution1 {
+    public int minDistance(String word1, String word2) {
+        return dp(word1, word2, word1.length() - 1, word2.length() - 1);
+    }
+
+    // 定义: dp(i, j) 返回 s1[0..i] 和 s2[0..j] 的最小编辑距离
+    private int dp(String word1, String word2, int i, int j) {
+
+        // base case
+        if (i == -1) {
+            return j + 1;
+        }
+        if (j == -1) {
+            return i + 1;
+        }
+
+        if (word1.charAt(i) == word2.charAt(j)) {
+            return dp(word1, word2, i - 1, j - 1); // 啥都不做
+        } else {
+            return min(
+                    dp(word1, word2, i, j - 1) + 1, // 插入
+                    dp(word1, word2, i - 1, j) + 1, // 删除
+                    dp(word1, word2, i - 1, j - 1) + 1 // 替换
+            );
+        }
+    }
+
+    private int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+}
