e490b8c8003e16f124c5e9f90fa730d805c1c5de
Vik1ang <vik1ang@outlook.com>
2021-11-27 21:11:37 +0800

    palindrome

---
+++ Algo/src/main/java/dp/subsequence/q516/longestPalindromeSubseq/Solution.java
@@ -0,0 +1,9 @@
+package dp.subsequence.q516.longestPalindromeSubseq;
+
+// https://leetcode-cn.com/problems/longest-palindromic-subsequence/
+
+public class Solution {
+    public int longestPalindromeSubseq(String s) {
+        return -1;
+    }
+}
---
+++ Algo/src/main/java/string/palindrome/q5/Solution.java
@@ -0,0 +1,9 @@
+package string.palindrome.q5;
+
+// https://leetcode-cn.com/problems/longest-palindromic-substring/
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
