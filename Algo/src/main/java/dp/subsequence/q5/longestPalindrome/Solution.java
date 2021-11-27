29dc4ae5eded99413b6bd43f303230e6d48892ba
Vik1ang <vik1ang@outlook.com>
2021-11-27 21:12:50 +0800

    move folder

---
+++ Algo/src/main/java/dp/subsequence/q5/longestPalindrome/Solution.java
@@ -0,0 +1,9 @@
+package dp.subsequence.q5.longestPalindrome;
+
+// https://leetcode-cn.com/problems/longest-palindromic-substring/
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
