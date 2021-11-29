056dc6d46d30a3d140248a52f3b0c611f43a4340
Vik1ang <vik1ang@outlook.com>
2021-11-29 16:59:45 +0800

    coin change

---
+++ Algo/src/main/java/dp/knapsack/q512/coinChange2/Solution.java
@@ -0,0 +1,9 @@
+package dp.knapsack.q512.coinChange2;
+
+// https://leetcode-cn.com/problems/coin-change-2/
+
+public class Solution {
+    public int change(int amount, int[] coins) {
+        return -1;
+    }
+}
---
+++ Algo/src/main/java/dp/knapsack/q512/coinChange2/Solution2.java
@@ -0,0 +1,21 @@
+package dp.knapsack.q512.coinChange2;
+
+// https://leetcode-cn.com/problems/coin-change-2/
+
+public class Solution2 {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j - coins[i] >= 0) {
+                    dp[j] = dp[j] + dp[j - coins[i]];
+                }
+            }
+        }
+
+        return dp[amount];
+    }
+}
