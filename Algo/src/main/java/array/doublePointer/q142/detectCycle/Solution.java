460870935e35a0fe64e97f2454bb65339b75f1eb
Vik1ang <vik1ang@outlook.com>
2021-11-30 16:10:26 +0800

    detect cycle

---
+++ Algo/src/main/java/array/doublePointer/q142/detectCycle/Solution.java
@@ -0,0 +1,11 @@
+package array.doublePointer.q142.detectCycle;
+
+// https://leetcode-cn.com/problems/linked-list-cycle-ii/
+
+import dataStructure.ListNode;
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        return null;
+    }
+}
---
+++ Algo/src/main/java/array/doublePointer/q142/detectCycle/Solution1.java
@@ -0,0 +1,34 @@
+package array.doublePointer.q142.detectCycle;
+
+// https://leetcode-cn.com/problems/linked-list-cycle-ii/
+
+import dataStructure.ListNode;
+
+public class Solution1 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast, slow;
+        fast = slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) {
+            // fast 遇到空指针说明没有环
+            return null;
+        }
+
+        slow = head;
+
+        while (slow != fast) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
