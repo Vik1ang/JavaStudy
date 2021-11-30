8f2701a2473d5474bfba3395260269357ace1e11
Vik1ang <vik1ang@outlook.com>
2021-11-30 16:11:00 +0800

    detect cycle

---
+++ Algo/src/main/java/linkedList/q142/detectCycle/Solution.java
@@ -0,0 +1,11 @@
+package linkedList.q142.detectCycle;
+
+// https://leetcode-cn.com/problems/linked-list-cycle-ii/
+
+import dataStructure.ListNode;
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        return null;
+    }
+}
