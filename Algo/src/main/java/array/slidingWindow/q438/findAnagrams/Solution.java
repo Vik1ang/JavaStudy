7cb23f2e58693024b02459cd876794e3ffb56395
Vik1ang <vik1ang@outlook.com>
2021-11-30 22:07:21 +0800

    find anagrams

---
+++ Algo/src/main/java/array/slidingWindow/q438/findAnagrams/Solution.java
@@ -0,0 +1,11 @@
+package array.slidingWindow.q438.findAnagrams;
+
+// https://leetcode-cn.com/problems/find-all-anagrams-in-a-string/
+
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        return null;
+    }
+}
---
+++ Algo/src/main/java/array/slidingWindow/q438/findAnagrams/Solution1.java
@@ -0,0 +1,55 @@
+package array.slidingWindow.q438.findAnagrams;
+
+// https://leetcode-cn.com/problems/find-all-anagrams-in-a-string/
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution1 {
+    public List<Integer> findAnagrams(String s, String p) {
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left =0, right = 0;
+        int valid = 0;
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+
+            // 进行窗口内数据的一系列更新
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            // 判断左侧窗口是否要收缩
+            while (right - left >= p.length()) {
+                // 当窗口符合条件时，把起始索引加入 res
+                if (valid == need.size()) {
+                    res.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                // 进行窗口内数据的一系列更新
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+
+        return res;
+    }
+}
