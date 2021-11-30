faa5000ecb9f3d987598aec889e4a2d2a3460221
Vik1ang <vik1ang@outlook.com>
2021-11-30 16:57:42 +0800

    two sum

---
+++ Algo/src/main/java/array/doublePointer/q167/twoSum/Solution.java
@@ -0,0 +1,9 @@
+package array.doublePointer.q167.twoSum;
+
+// https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        return null;
+    }
+}
---
+++ Algo/src/main/java/array/doublePointer/q167/twoSum/Solution1.java
@@ -0,0 +1,22 @@
+package array.doublePointer.q167.twoSum;
+
+// https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+
+public class Solution1 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
