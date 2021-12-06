cf8b677b112bb5deeca65714dd088aec5f5309a8
Vik1ang <vik1ang@outlook.com>
2021-12-06 21:55:51 +0800

    max profit

---
+++ Algo/src/main/java/dp/stock/q714/maxProfit/Solution.java
@@ -0,0 +1,9 @@
+package dp.stock.q714.maxProfit;
+
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+public class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        return 0;
+    }
+}
---
+++ Algo/src/main/java/dp/stock/q714/maxProfit/Solution2.java
@@ -0,0 +1,16 @@
+package dp.stock.q714.maxProfit;
+
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+public class Solution2 {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i] - fee);
+        }
+        return dp_i_0;
+    }
+}
