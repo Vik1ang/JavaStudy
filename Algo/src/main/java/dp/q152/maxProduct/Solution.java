6b0ab48d2118ae6b26f0827487622ca82cecc0a2
Vik1ang <vik1ang@outlook.com>
2021-12-07 02:45:38 +0800

    max product

---
+++ Algo/src/main/java/dp/q152/maxProduct/Solution.java
@@ -0,0 +1,9 @@
+package dp.q152.maxProduct;
+
+// https://leetcode-cn.com/problems/maximum-product-subarray/
+
+public class Solution {
+    public int maxProduct(int[] nums) {
+        return 0;
+    }
+}
