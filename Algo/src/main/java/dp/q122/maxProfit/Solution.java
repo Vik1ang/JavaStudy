04019dbab4473016f292a9f8b5245412d7ac8b37
Vik1ang <vik1ang@outlook.com>
2021-12-06 21:33:54 +0800

    max profit II

---
+++ Algo/src/main/java/dp/q122/maxProfit/Solution.java
@@ -0,0 +1,9 @@
+package dp.q122.maxProfit;
+
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+}
---
+++ Algo/src/main/java/dp/q122/maxProfit/Solution2.java
@@ -0,0 +1,16 @@
+package dp.q122.maxProfit;
+
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+public class Solution2 {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
+    }
+}
