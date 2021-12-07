4c33cb1ab9595e8f7ec5a4dc1bff8070238c6462
Vik1ang <vik1ang@outlook.com>
2021-12-07 23:00:48 +0800

    wiggle max length

---
+++ Algo/src/main/java/dp/q376/wiggleMaxLength/Solution.java
@@ -0,0 +1,9 @@
+package dp.q376.wiggleMaxLength;
+
+// https://leetcode-cn.com/problems/wiggle-subsequence/
+
+public class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        return 0;
+    }
+}
