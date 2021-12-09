dcbb68c10361e114a21d914483e8297b47724262
Vik1ang <vik1ang@outlook.com>
2021-12-09 10:22:28 +0800

    commit

---
+++ Algo/src/main/java/unclassified/q749/containVirus/Solution.java
@@ -0,0 +1,9 @@
+package unclassified.q749.containVirus;
+
+// https://leetcode-cn.com/problems/contain-virus/
+
+public class Solution {
+    public int containVirus(int[][] isInfected) {
+        return 0;
+    }
+}
