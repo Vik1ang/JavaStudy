524e2204acac80e35b98ca6d03bd8bb5bb701d9f
Vik1ang <vik1ang@outlook.com>
2022-01-06 22:25:26 +0800

    jvm

--- JVM_Study/src/main/java/chapter02/ClassLoaderTest1.java
+++ JVM_Study/src/main/java/chapter02/ClassLoaderTest1.java
@@ -4,11 +4,11 @@ import java.net.URL;
 
 public class ClassLoaderTest1 {
     public static void main(String[] args) {
-        System.out.println("#####启动类加载器#####");
-        URL[] urLs = sun.misc.Launcher.getBootstrapClassPath().getURLs();
-        for (URL urL : urLs) {
-            System.out.println(urL.toExternalForm());
-        }
+        // System.out.println("#####启动类加载器#####");
+        // URL[] urLs = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        // for (URL urL : urLs) {
+        //     System.out.println(urL.toExternalForm());
+        // }
 
         // System.out.println("#####扩展类加载器#####");
         // String extDirs = System.getProperty("java.ext.dirs");
